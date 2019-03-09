a89394acad1f2f4d42b06f5316931e78187cab55
pumbaa175 <47711740+pumbaa175@users.noreply.github.com>
2019-03-09 23:04:13 +0700

    last days

--- FireFlane/src/com/example/processed/BOTProcessed.java
+++ FireFlane/src/com/example/processed/BOTProcessed.java
@@ -5,10 +5,102 @@
  */
 package com.example.processed;
 
+import com.example.data.Player;
+import com.example.userinterface.TablePosition;
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  *
  * @author pumba
  */
 public class BOTProcessed {
     
+    private final int TYPE_RANDOM = 111;
+    private final int TYPE_FIRE_1 = 222;
+    
+    private ArrayList<Player> alPlane;
+    private ArrayList<Player> alFirePlane;
+    private int countColumn = TablePosition.COUNT_COLUMN;
+    private int typeFire = TYPE_RANDOM;
+    private boolean isFire = false;
+    private int selectColumn = 5;
+    private int selectRow = 5;
+    
+    public BOTProcessed() {
+        
+    }
+    
+    public void choosePlane(){
+        Random random = new Random();
+        alPlane = new ArrayList<>();
+        int cell;
+        while(alPlane.size() < 5){
+            int column = random.nextInt(10) + 1; //1-10
+            int row = random.nextInt(10); //0-9
+            cell = row *(countColumn - 1) + column;
+            for(int i = 0; i < alPlane.size(); i++){
+                if (cell == alPlane.get(i).getCell()){
+                    continue;
+                }
+            }
+            alPlane.add(new Player(column, row, countColumn));
+        }
+        
+    }
+
+    public void firePlane(){
+        switch(typeFire){
+            case TYPE_RANDOM:
+                while(!firePlaneRandom()){}
+                break;
+            case TYPE_FIRE_1:
+                while(!firePlaneNo1EasyMode(selectColumn, selectRow));
+                break;
+        }
+        
+    }
+    
+    private boolean firePlaneRandom(){
+        Random random = new Random();
+        int column = random.nextInt(8) + 2;  //2-9
+        int row = random.nextInt(8) + 1; //1-8
+        //Checking
+        if(!checkDuplicate(column, row)){
+            alFirePlane.add(new Player(column, row, countColumn));
+            return true;
+        }
+        return false;
+    }
+    
+    private boolean firePlaneNo1EasyMode(int selectColumn, int selectRow){
+        Random random = new Random();
+        int column = random.nextInt(3) + selectColumn - 1; //player.getcolumn - 1 ---> player.getcolumn +1
+        int row = random.nextInt(3) + selectRow - 1; //player.getrow - 1 ---> player.getrow +1
+        if(!checkDuplicate(column, row)){
+            alFirePlane.add(new Player(column, row, countColumn));
+            return true;
+        }
+        return false;
+    }
+    
+    private boolean checkFireType(){
+        
+        return false;
+    }
+    
+    private boolean checkDuplicate(int column, int row){
+        int cell = row *(countColumn - 1) + column;
+        for(Player p : alPlane){
+            if (cell == p.getCell()){
+                return true;
+            }
+        }
+        for(Player p : alFirePlane){
+            if(cell == p.getCell()){
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- FireFlane/src/com/example/processed/Processed.java
+++ FireFlane/src/com/example/processed/Processed.java
@@ -6,7 +6,9 @@
 package com.example.processed;
 
 import com.example.data.Player;
+import com.example.data.Position;
 import com.example.userinterface.TablePosition;
+import java.util.ArrayList;
 
 /**
  *
@@ -34,34 +36,45 @@ public class Processed {
         return enemyPlayer;
     }
 
-    public void hintsLog() {
-        int fireCell = 15;
-        int[] plane = {26};
-        if (hintsLogNo1(fireCell, plane)) {
+    public void hintsLog(Position position, ArrayList<Position> alPosition) {
+//        int fireCell = 15;
+//        int[] plane = {26};
+//        if (hintsLogNo1(fireCell, plane)) {
+//            System.out.println("Enermy Plane stay in area 3x3");
+//        }
+        if(hintsLogNo1(position, alPosition)){
             System.out.println("Enermy Plane stay in area 3x3");
         }
     }
 
-    private boolean hintsLogNo1(int fireCell, int[] plane) {
-        for (int i : plane) {
-            if ((fireCell - countCol < i) && (i < fireCell + countCol)) {
-                if (fireCell == (i + 1) || fireCell == (i - 1)) {
-                    return true;
-                }
-                if (fireCell == (i - countCol + 1) || fireCell == (i - countCol) || fireCell == (i - countCol + 2)) {
-                    return true;
-                }
-                if (fireCell == (i + countCol - 1) || fireCell == (i + countCol - 2) || fireCell == (i + countCol)) {
+//    private boolean hintsLogNo1(int fireCell, int[] plane) {
+//        for (int i : plane) {
+//            if ((fireCell - countCol < i) && (i < fireCell + countCol)) {
+//                if (fireCell == (i + 1) || fireCell == (i - 1)) {
+//                    return true;
+//                }
+//                if (fireCell == (i - countCol + 1) || fireCell == (i - countCol) || fireCell == (i - countCol + 2)) {
+//                    return true;
+//                }
+//                if (fireCell == (i + countCol - 1) || fireCell == (i + countCol - 2) || fireCell == (i + countCol)) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+//    }
+    
+    private boolean hintsLogNo1(Position position, ArrayList<Position> alPosition){
+        
+        for(Position p : alPosition){
+            if(position.getColumn() - 1 <= p.getColumn() && p.getColumn() <= position.getColumn() + 1){
+                if(position.getRow()- 1 <= p.getRow()&& p.getRow() <= position.getRow()+ 1){
                     return true;
                 }
             }
         }
         return false;
     }
-
-    public static void main(String[] args) {
-        Processed processed = new Processed();
-        processed.hintsLog();
-    }
-
+    
+    
 }
